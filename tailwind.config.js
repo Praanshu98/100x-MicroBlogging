ec0555961640adfde0247ec67edda087be6b0041
Praanshu Grover <praanshugrover60@gmail.com>
2023-10-01 14:40:16 +0530

    Adds Button component code and add links to all pages on index page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,30 +6,54 @@ module.exports = {
       'inter': ['Inter', 'sans-serif'],
       'chirp': ['Chirp']
     },
-    borderRadius: {
-      'button': '65px'
-    },
     extend: {
+      borderRadius: {
+        'button-l': "30px",
+        'button-xl': "65px",
+      },
+      padding: {
+        '93px': '93px',
+      },
       colors: {
-        'Neutral-50': '#F9F9F9',
-        'Neutral-100': '#F4F4F4',
-        'Neutral-200': '#E4E4E4',
-        'Neutral-300': '#D3D3D3',
-        'Neutral-400': '#A2A2A2',
-        'Neutral-500': '#737373',
-        'Neutral-600': '#525252',
-        'Neutral-700': '#404040',
-        'Neutral-800': '#262626',
-        'Neutral-900': '#171717',
-        'Neutral-950': '#0A0A0A',
-        'Neutral-1000': '#000000',
-        'Twitter-Blue': '#1D9BF0',
+        'Neutral':{
+          '50': '#F9F9F9',
+          '100': '#F4F4F4',
+          '200': '#E4E4E4',
+          '300': '#D3D3D3',
+          '400': '#A2A2A2',
+          '500': '#737373',
+          '600': '#525252',
+          '700': '#404040',
+          '800': '#262626',
+          '900': '#171717',
+          '950': '#0A0A0A',
+          '1000': '#000000'
+        },
+        'Twitter-Blue': {
+          'Default': '#1D9BF0',
+          'Hover': '#1871CA',
+          'Disabled': '#1E5D87',
+        },
+        'Blue-Wash': 'rgba(117, 190, 239, 0.20)',
+        'Button-Stroke': 'rgba(29, 155, 240, 0.24)',
+        'Searchbar-Fill': '#212327',
+        'Card-Fill': '#16181C',
+        'Success': '#00BE74',
+        'Error': '#8B141A',
+
       },
       boxShadow: {
         'button': '0px 8px 16px 0px rgba(0, 0, 0, 0.25)',
       },
+      width:{
+        98.5:'394px',
+        16.5:'66px',
+        23.5:'94px',
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 
